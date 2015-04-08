6f10ac9fb09a9bcce60dc24e983ddaddb7b5036f
crystal <gisele6@126.com>
2015-04-08 18:04:53 +0800

    ng-app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ angular
   .module('p2pSiteMobApp', [
     'ngAnimate',
     'ngTouch',
-    'famous.angular',
+    // 'famous.angular',
     'ui.router'
   ])
   .config(function($stateProvider, $urlRouterProvider) {
