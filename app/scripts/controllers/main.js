319572f2f1136602d68ab5d2cc2f6856c2feda6c
wangkaijie <wangkaijie@hoolai.com>
2015-04-10 10:30:17 +0800

    no message

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,17 @@
  * Controller of the p2pSiteMobApp
 + */
 angular.module('p2pSiteMobApp')
-  .controller('MainCtrl',['$scope', 'User', 'HongcaiUser', function ($scope, User, HongcaiUser) {
+  .controller('MainCtrl',['$scope', 'restmod', 'User', 'HongcaiUser', function ($scope, restmod, User, HongcaiUser) {
     $scope.user = User.$find(1);
     $scope.oneHongcaiUser = HongcaiUser.$find(402016);
+
+    var loginAuth = restmod.model('/ipa/hongcai/users/login');
+    var loginHongcaiAuth = restmod.model('/hongcai/rest/base/login');
+    $scope.login = function(user) {
+      // 宏财用户
+      // return loginHongcaiAuth.$create({account: user.account, password: user.password});
+      // 微信用户登录
+      return loginAuth.$create({account: user.account, password: user.password})
+    }
+
   }]);
