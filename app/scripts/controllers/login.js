f75e66073246522c204227e0942e0c4bcec7810d
wangkaijie <wangkaijie@hoolai.com>
2015-04-13 10:49:30 +0800

    删除多余的调试代码

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,7 +11,6 @@ angular.module('p2pSiteMobApp')
   .controller('LoginCtrl', ['$scope', '$stateParams', 'md5', 'Login', 'HongcaiLogin', function ($scope, $stateParams, md5, Login, HongcaiLogin) {
     // 获取用户的openId
     var openId = $stateParams.openId;
-    console.log(openId);
     $scope.toLogin = function(user) {
       if (openId === undefined || openId === '' || openId === null) {
         HongcaiLogin.$create({account: user.account, password: md5.createHash(user.password)})
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,17 +7,5 @@
  * Controller of the p2pSiteMobApp
 + */
 angular.module('p2pSiteMobApp')
-  .controller('MainCtrl',['$scope', 'restmod', 'User', 'HongcaiUser', function ($scope, restmod, User, HongcaiUser) {
-    // 暂时注释掉
-    // $scope.user = User.$find(1);
-    // $scope.oneHongcaiUser = HongcaiUser.$find(402016);
-    // var loginAuth = restmod.model('/ipa/hongcai/users/login');
-    // var loginHongcaiAuth = restmod.model('/hongcai/rest/base/login');
-    // $scope.login = function(user) {
-    //   // 宏财用户
-    //   // return loginHongcaiAuth.$create({account: user.account, password: user.password});
-    //   // 微信用户登录
-    //   return loginAuth.$create({account: user.account, password: user.password})
-    // }
-
-  }]);
+  .controller('MainCtrl', function ($scope) {
+  });
