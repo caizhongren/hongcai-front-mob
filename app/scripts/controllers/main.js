3a0e23b43062bb7ed3656bc105d8aa7a018be83f
crystal <gisele6@126.com>
2015-04-27 15:19:50 +0800

    首页-数据

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,16 +12,31 @@ angular.module('p2pSiteMobApp')
     // 获取宏金宝投资列表
     // $scope.projectsRecommendations = projects.$find('recommendations');
     // 获取宏金盈投资列表
-    $scope.fundsProjectsRecommendations = fundsProjects.$find('recommendations');
+    $scope.fundsProjects = fundsProjects.$find('recommendations');
+    console.log($scope.fundsProjects);
     $scope.toggle = {};
     $scope.tabs = [{
       title: '7日理财',
+      content: $scope.fundsProjects[0]
     }, {
-      title: '1-6月理财',
+      title: '1-6月理财'
+    }];
+    $scope.tabs_sub = [{
+      title: '月月盈',
+      content: $scope.fundsProjects[1]
+    }, {
+      title: '季度盈',
+      content: $scope.fundsProjects[2]
+    }, {
+      title: '半年盈',
+      content: $scope.fundsProjects[3]
     }];
 
     $scope.toggle.switchTab = function(tabIndex) {
       $scope.toggle.activeTab = tabIndex;
+    };
+    $scope.toggle.switchSubTab = function(subTabIndex) {
+      $scope.toggle.activeSubTab = subTabIndex;
     };
 
   }]);
