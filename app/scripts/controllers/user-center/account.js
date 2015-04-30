318de545a752b7cfe67d0b5aba65065269d29aa9
wangkaijie <wangkaijie@hoolai.com>
2015-04-30 15:19:36 +0800

    我的账户- 添加退出按钮。

--- app/scripts/controllers/user-center/account.js
+++ app/scripts/controllers/user-center/account.js
@@ -8,7 +8,7 @@
  * Controller of the p2pSiteMobApp
  */
 angular.module('p2pSiteMobApp')
-  .controller('AccountCtrl', ['$scope', '$rootScope', 'HongcaiUser',function ($scope, $rootScope, HongcaiUser) {
+  .controller('AccountCtrl', ['$scope', '$rootScope', '$state', 'HongcaiUser', 'restmod', 'DEFAULT_DOMAIN', function ($scope, $rootScope, $state, HongcaiUser, restmod, DEFAULT_DOMAIN) {
     if ($rootScope.hasLoggedUser) {
       HongcaiUser.$find($rootScope.hasLoggedUser.id + '/account').$then(function(response) {
         if (response.$status === 'ok') {
@@ -19,4 +19,21 @@ angular.module('p2pSiteMobApp')
         }
       });
     }
+
+
+    // 退出登录功能
+    $scope.toLogout = function() {
+      // var
+      if ($rootScope.hasLoggedUser) {
+        // TODO  登出的model在这里不太好吧。
+        var logoutModel = restmod.model(DEFAULT_DOMAIN + '/users/' + $rootScope.hasLoggedUser.id + '/logout');
+        logoutModel.$create().$then(function(response) {
+          if (response.ret === 1) {
+            $rootScope.hasLoggedUser = null;
+            $rootScope.isLogged = false;
+            $state.go('root.main');
+          }
+        });
+      }
+    };
   }]);
