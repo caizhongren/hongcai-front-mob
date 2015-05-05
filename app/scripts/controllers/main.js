38cd16bd71d3685fa51a1c8648b443063f81f2d6
crystal <gisele6@126.com>
2015-05-05 17:01:00 +0800

    首页tab切换添加注释

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -44,9 +44,8 @@ angular.module('p2pSiteMobApp')
       $scope.toggle.activeTab = tabIndex;
       var type = tabIndex + 1;
       $scope.switchFundsProjects(type);
-      if(tabIndex === 1){
-        $scope.toggle.switchSubTab(0);
-      }
+      //初始化第二层Tab数据
+      tabIndex === 1?$scope.toggle.switchSubTab(0):'';
     };
     $scope.toggle.switchSubTab = function(subTabIndex) {
       $scope.toggle.activeSubTab = subTabIndex;
