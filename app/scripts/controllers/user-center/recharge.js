d39161f8f6072ced4bd0c5fa0f6882b4815b35b3
crystal <gisele6@126.com>
2015-05-12 17:21:16 +0800

    充值-交互

--- app/scripts/controllers/user-center/recharge.js
+++ app/scripts/controllers/user-center/recharge.js
@@ -8,16 +8,31 @@
  * Controller of the p2pSiteMobApp
  */
 angular.module('p2pSiteMobApp')
-  .controller('RechargeCtrl', ['$scope', '$rootScope', 'HongcaiUser', function ($scope, $rootScope, HongcaiUser) {
+  .controller('RechargeCtrl', ['$scope', '$rootScope', 'HongcaiUser', 'restmod', 'DEFAULT_DOMAIN', function ($scope, $rootScope, HongcaiUser, restmod, DEFAULT_DOMAIN) {
     $rootScope.selectedSide =  'account';
+    $scope.rechargeAmount = '';
     if ($rootScope.hasLoggedUser) {
-      HongcaiUser.$find($rootScope.hasLoggedUser.user.id + '/recharge').$then(function(response) {
+      HongcaiUser.$find($rootScope.hasLoggedUser.user.id + '/account').$then(function(response) {
         if (response.$status === 'ok') {
-          // 获取用户充值信息
-          $scope.simpleRecharge = response;
+          // 获取用户金额信息
+          $scope.userAccount = response;
+          // 充值
+          $scope.recharge = function(){
+            var rechargeModel = restmod.model(DEFAULT_DOMAIN + '/users/' + $rootScope.hasLoggedUser.user.id + '/recharge');
+            rechargeModel.$create({'amount':$scope.rechargeAmount}).$then(function(response) {
+              if (response.$status === 'ok') {
+                // 获取用户充值信息
+                $scope.simpleRecharge = response;
+              } else {
+                  // 获取信息失败。
+              }
+            });
+          };
         } else {
           // 获取信息失败。
         }
       });
+
+      
     }
   }]);
