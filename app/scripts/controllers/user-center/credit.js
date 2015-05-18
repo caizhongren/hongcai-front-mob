1211c04a7c4b83febc4b3232e9383dfeed9e68bd
wangkaijie <wangkaijie@hoolai.com>
2015-05-18 18:31:48 +0800

    credit 我的债权

--- app/scripts/controllers/user-center/credit.js
+++ app/scripts/controllers/user-center/credit.js
@@ -9,7 +9,7 @@
  */
 angular.module('p2pSiteMobApp')
   .controller('CreditCtrl', ['$scope', '$rootScope', '$state', 'HongcaiUser', 'restmod', 'DEFAULT_DOMAIN', function($scope, $rootScope, $state, HongcaiUser, restmod, DEFAULT_DOMAIN) {
-    
+
     // tab
     $scope.toggle = {};
     $scope.tabs = [{
@@ -19,20 +19,34 @@ angular.module('p2pSiteMobApp')
     }];
 
     $rootScope.selectedSide =  'investments-stat';
+    $scope.page = 1;
+    $scope.pageSize = 10;
 
     $scope.toggle.switchTab = function(tabIndex) {
       $scope.toggle.activeTab = tabIndex;
+      if (tabIndex === 0) {
+        $scope.creditStatus = '1,2';
+      } else {
+        $scope.creditStatus = '3';
+      }
+      $scope.getCredits($scope.creditStatus);
     };
 
-    if ($rootScope.hasLoggedUser) {
-      HongcaiUser.$find($rootScope.hasLoggedUser.id + '/credits').$then(function(response) {
-        if (response.$status === 'ok') {
-          $scope.simpleCredits = response;
-        } else {
-          //
-        }
-      });
-      // 接口还没部署到43
-      // console.log($scope.simpleCredits);
-    }
+    $scope.getCredits = function(status) {
+      if ($rootScope.hasLoggedUser) {
+        $scope.creditStatus = status;
+        $scope.credits = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/credits', {
+          status: $scope.creditStatus,
+          page: $scope.page,
+          pageSize: $scope.pageSize
+        });
+      }
+    };
+
+    $scope.loadCreditMore = function() {
+      $scope.pageSize = $scope.pageSize + 10;
+      $scope.getCredits($scope.creditStatus);
+    };
+
+    $scope.getCredits('1,2');
   }]);
