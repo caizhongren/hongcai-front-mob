51f27be3239137b3e2223b4fbb91debf5718263f
wangyadan <wangyadan@hoolai.com>
2015-08-11 18:34:36 +0800

    我的交易记录自动加载测试

--- app/scripts/controllers/user-center/deal.js
+++ app/scripts/controllers/user-center/deal.js
@@ -11,7 +11,9 @@ angular.module('p2pSiteMobApp')
   .controller('DealCtrl', ['$rootScope', '$scope','$state', 'HongcaiUser', function($rootScope, $scope, $state,HongcaiUser) {
     $rootScope.selectedSide = 'account';
     $scope.page = 1;
-    $scope.pageSize = 10;
+    $scope.pageSize = 20;
+    $scope.deals = [];
+    $scope.totalPage = 1;
     // if ($rootScope.hasLoggedUser) {
     //   $scope.deals = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/deals', {
     //     page: $scope.page,
@@ -20,42 +22,69 @@ angular.module('p2pSiteMobApp')
     //  // $scope.DealBusy = false;
     // }
     //自动加载更多
-    $scope.getDealList = function(){
-      $scope.deals = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/deals', {
+    //
+    //
+    //
+    //
+    $scope.dealList = function(){
+      if ($scope.totalPage < $scope.page){
+        return;
+      }
+      var dealsReq = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/deals', {
         page: $scope.page,
         pageSize: $scope.pageSize
       });
-      $scope.deals.$then(function(response){
+      dealsReq.$then(function(response){
         if(response.$status === 'ok'){
-          console.log(response);
-            $scope.deals.data= response.data;
-            console.log($scope.deals.data);
-        } else{
+          $scope.totalPage = response.totalPage;
+          for (var i = 0; i < response.data.length; i++) {
+            $scope.deals.push(response.data[i]);
+          };
+       } else{
             $scope.msg = '获取信息失败';
         }
       });
-     //$scope.DealBusy = true;
+     //$scope.DealBusy = false;
     }
 
-    $scope.getDealList();
+    // $scope.getDealList = function(){
+    //   $scope.deals = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/deals', {
+    //     page: $scope.page,
+    //     pageSize: $scope.pageSize
+    //   });
+    //   $scope.deals.$then(function(response){
+    //     if(response.$status === 'ok'){
+    //       console.log(response);
+    //         $scope.deals.data= response.data;
+    //         console.log($scope.deals.data);
+    //     } else{
+    //         $scope.msg = '获取信息失败';
+    //     }
+    //   });
+    //  //$scope.DealBusy = false;
+    // }
+
+    $scope.dealList();
 
     $scope.loadDealMuch = function(){
       $scope.DealBusy = true;
-      $scope.pageSize = $scope.pageSize + 10;
+      $scope.page = $scope.page + 1;
+      $scope.pageSize = $scope.pageSize;
       console.log($scope.pageSize);
-      $scope.getDealList();
+      $scope.dealList();
+      $scope.DealBusy = false;
     };
     //以上为增加 自动加载代码
 
     // 通过点击加载更多宏金盈项目
     
-    $scope.loadDealMore = function() {
-      $scope.pageSize = $scope.pageSize + 10;
-      $scope.deals = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/deals', {
-        page: $scope.page,
-        pageSize: $scope.pageSize
-      })
-    };
+    // $scope.loadDealMore = function() {
+    //   $scope.pageSize = $scope.pageSize + 10;
+    //   $scope.deals = HongcaiUser.$find($rootScope.hasLoggedUser.id + '/deals', {
+    //     page: $scope.page,
+    //     pageSize: $scope.pageSize
+    //   })
+    // };
     
 
     $scope.dealMap = {
