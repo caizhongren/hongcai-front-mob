3ec5ff32989c2e46d3db0fdbd2b106ee8a3947fe
wangyadan <wangyadan@hoolai.com>
2015-11-26 16:17:54 +0800

    ng-src

--- app/scripts/controllers/activity/experience-activity-ctrl.js
+++ app/scripts/controllers/activity/experience-activity-ctrl.js
@@ -4,13 +4,13 @@ angular.module('p2pSiteMobApp')
   .controller('ExperienceActivityCtrl', function($rootScope, $scope, $state, $stateParams) {
     
     $scope.number = $stateParams.number;
-    $scope.imgSrc = "activity0071.png";
+    $scope.imgSrc = "/images/activity/activity0071.png";
 
     if ($scope.number === "1") {
-      $scope.imgSrc = "activity0071.png";
+      $scope.imgSrc = "/images/activity/activity0071.png";
       $scope.moneyTest = "2000";
     } else if ($scope.number === "2") {
-      $scope.imgSrc = "activity0072.png";
+      $scope.imgSrc = "/images/activity/activity0072.png";
       $scope.moneyTest = "4000";
     }
 
