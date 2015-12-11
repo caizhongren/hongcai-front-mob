d499d3ce0bfc291c1591062963bb1a70f0ac7088
zhangfeng <zhangfeng@hoolai.com>
2015-12-11 10:41:23 +0800

    测试动态title

--- app/scripts/controllers/get-pwd-back.js
+++ app/scripts/controllers/get-pwd-back.js
@@ -9,7 +9,7 @@
  */
 angular.module('p2pSiteMobApp')
   .controller('GetPwdCtrl', function($rootScope, $scope, $state, $stateParams, $location, md5, register, wechat, mobileCaptcha, HongcaiUser, restmod, DEFAULT_DOMAIN) {
-
+    $rootScope.headerTitle = '找回密码';
     // 用户获取手机验证码
     $scope.sendMobileCaptcha = function(user) {
       mobileCaptcha.$create({
