6bed3884bab00bfa45e85a2f4eab56cdee2bbc43
wangyadan <wangyadan@hoolai.com>
2016-01-27 10:48:48 +0800

    零存宝详情页状态优化测试1

--- app/scripts/controllers/project/funds-project-detail.js
+++ app/scripts/controllers/project/funds-project-detail.js
@@ -8,7 +8,7 @@
  * Controller of the p2pSiteMobApp
  */
 angular.module('p2pSiteMobApp')
-  .controller('FundsProjectDetailCtrl', function($scope, $state, $rootScope, $stateParams, fundsProjects, Restangular, restmod, DEFAULT_DOMAIN, config) {
+  .controller('FundsProjectDetailCtrl', function($scope, $state, $rootScope, $stateParams,$location, fundsProjects, Restangular, restmod, DEFAULT_DOMAIN, config) {
     // 宏金盈详情页面
     var number = $stateParams.number;
     if (!number) {
@@ -165,6 +165,31 @@ angular.module('p2pSiteMobApp')
       $state.go('root.yeepay-transfer', {
         type: 'autoTransfer',
         number: "null"
+      });
+    }
+    /*
+    * 带参数跳至登录页
+    */
+    $scope.toLog = function() {
+      var redirectUrl = $location.path();
+      $state.go('root.login', {
+        redirectUrl: redirectUrl
+      });
+      return;
+    }
+
+    /**
+     * 实名认证，即开通易宝
+     */
+    $scope.realNameAuth = function(user){
+      if (!user.realName || !user.idNo){
+        $scope.errMsg = '请输入姓名或身份证号';
+      }
+      $state.go('root.yeepay-transfer', {
+        type: 'register',
+        number: "null",
+        realName: user.realName,
+        idNo: user.idNo
       });
     }
 
