7c53fa3af96cc2e831048017e9963ea972253709
liujia <liujia@hoolai.com>
2016-02-23 17:53:52 +0800

    feat(freeWish): bind share-spring-home data bind share-spring-home data

--- app/scripts/controllers/activity/share-spring-home-ctrl.js
+++ app/scripts/controllers/activity/share-spring-home-ctrl.js
@@ -5,15 +5,14 @@ angular.module('p2pSiteMobApp')
     $rootScope.showFooter = false;
     $scope.act = $stateParams.act;
     $scope.channelCode = $stateParams.f;
-    
-    $rootScope.showButton = false;
-    $scope.test = config.test;
 
+    $scope.test = config.test;
     $scope.coverLayerFlag = false;
     $scope.level = 1;
     //freeWishStatics.status:1.第一关，2.第一关结束，3第二关，4第二关结束，5第三关，6第三关结束
     //buttonFlag:1.我要现金2.我的任务3.开启第二关4.开启第三关5.我的账户
     $scope.buttonFlag = 1;
+    $scope.buttonValue = "我要现金";
     $rootScope.checkSession.promise.then(function(){
         if($rootScope.userInfo.id > 0){
           Restangular.one('freeWishes').one('freeWishStatics').get().then(function(response){
@@ -23,31 +22,37 @@ angular.module('p2pSiteMobApp')
                   //正在进行第一关任务，点击按钮进入第一关任务
                   $scope.level = 1;
                   $scope.buttonFlag = 2;
+                  $scope.buttonValue = "我要现金";
                 }else if($scope.freeWishStatics.status == 2){
                   //第一关完成，点击按钮开启第二关
                   $scope.level = 2;
                   $scope.buttonFlag = 3;
+                  $scope.buttonValue = "到账" +freeWishStatics.receiveAmount+"元，再拿30元";
                 }else if($scope.freeWishStatics.status == 3){
                   //正在进行第二关任务，点击按钮进入第二关任务
                   $scope.level = 2;
                   $scope.buttonFlag = 2;
+                  $scope.buttonValue = "到账" +freeWishStatics.receiveAmount+"元，再拿30元";
                 }else if($scope.freeWishStatics.status == 4){
                   //第二关完成，点击按钮开启第三关任务
                   $scope.level = 3;
                   $scope.buttonFlag = 4;
+                  $scope.buttonValue = "到账" +freeWishStatics.receiveAmount+"元，再拿100元";
                 }else if($scope.freeWishStatics.status == 5){
                   //正在进行第三关任务，点击按钮进入第三关任务
                   $scope.level = 3;
                   $scope.buttonFlag = 2;
+                  $scope.buttonValue = "到账" +freeWishStatics.receiveAmount+"元，再拿100元";
                 }else if($scope.freeWishStatics.status == 6){
                   //全部通关，点击按钮进入我的账户
                   $scope.level = 3;
                   $scope.buttonFlag = 5;
+                  $scope.buttonValue = "查看我的账户";
                 }
             }
           });
         }
-        
+
         if ($scope.channelCode){
           Restangular.one('freeWishes').post('channel', {
             openId: $rootScope.openid, 
@@ -62,6 +67,17 @@ angular.module('p2pSiteMobApp')
     Restangular.one('freeWishes').one('totalReward').get().then(function(response){
       $scope.totalReward = response;
     });
+
+
+    $scope.buttonClick = function(buttonFlag){
+      if(buttonFlag == 1 || buttonFlag == 3 || buttonFlag == 4){
+        $scope.openFreeWish();
+      }else if(buttonFlag == 2){
+        $scope.goShareDetail();
+      }else if(buttonFlag == 5){
+        $scope.goAccount();
+      }
+    }
 
     /**
      * 领取免费愿望
