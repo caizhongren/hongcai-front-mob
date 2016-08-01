8fa1ec654f20310f6d25b3eac321f09164f99c6a
zhangfeng <zhangfeng@hoolai.com>
2016-08-01 19:27:42 +0800

    test(fast click)：fastclick要先于其他js加载才可以

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -850,11 +850,11 @@ p2pSiteMobApp
 
   }])
   .run(function($rootScope, DEFAULT_DOMAIN, $q, $timeout, $state, $location, $http, $uibModal, ipCookie, restmod, config, Restangular, URLService, Utils) {
-    if ('addEventListener' in document) {
-        document.addEventListener('DOMContentLoaded', function() {
+    // if ('addEventListener' in document) {
+        // document.addEventListener('DOMContentLoaded', function() {
             FastClick.attach(document.body);
-        }, false);
-    }
+        // }, false);
+    // }
     
     Restangular.setBaseUrl('/hongcai/rest');
     Restangular.setDefaultHeaders({
