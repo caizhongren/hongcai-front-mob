bf9edadaa53b3999c621d8c7664f7b4fa44d10d3
fuqiang <fuqiang1@hoolai.com>
2016-08-05 15:07:36 +0800

    feat(bankcard) 银行卡解绑页面

--- app/scripts/controllers/user-center/bankcard.js
+++ app/scripts/controllers/user-center/bankcard.js
@@ -8,10 +8,10 @@
  * Controller of the p2pSiteMobApp
  */
 angular.module('p2pSiteMobApp')
-  .controller('BankcardCtrl',['$scope', '$rootScope', 'HongcaiUser', function ($scope, $rootScope, HongcaiUser) {
+  .controller('BankcardCtrl', ['$scope', '$rootScope', '$state', 'HongcaiUser','restmod','WEB_DEFAULT_DOMAIN', function($scope, $rootScope, $state, HongcaiUser,restmod, WEB_DEFAULT_DOMAIN) {
 
-    $rootScope.checkSession.promise.then(function(){
-      if(!$rootScope.isLogged){
+    $rootScope.checkSession.promise.then(function() {
+      if (!$rootScope.isLogged) {
         $state.go('root.login');
       }
 
@@ -22,6 +22,34 @@ angular.module('p2pSiteMobApp')
         } else {
           // 获取信息失败。
         }
+
+        var unBindBankcardModel = restmod.model(WEB_DEFAULT_DOMAIN + '/yeepay');
+        $scope.unBindBankcard = function() {
+          $scope.showBankCard = false;
+          unBindBankcardModel.$find('/unbindBankCard').$then(function(response) {
+            if(response.ret === -1) {
+              retrun;
+            }
+
+          });
+        }
+      });
+    });
+
+    $scope.showBankCard = false;
+    $scope.toRemoveCard = function() {
+      $scope.showBankCard = true;
+    }
+    $scope.cancle = function() {
+        $scope.showBankCard = false;
+      }
+      /*根据屏幕高度设置内容高度*/
+    angular.element('document').ready(function() {
+      //初始化宽度、高度
+      angular.element(".bankcard-body").css("min-height", angular.element(window).height() + "px");
+      //当文档窗口发生改变时 触发
+      angular.element(window).resize(function() {
+        angular.element(".bankcard-body").css("min-height", angular.element(window).height() + "px");
       });
     });
   }]);
