15de8ceb88f83a4b27a83f4b33a195e1d70a3614
fuqiang <fuqiang1@hoolai.com>
2016-08-11 17:05:48 +0800

    fix(债权详情) 宏金宝项目回款显示的判断条件

--- app/scripts/controllers/user-center/credit-security-details.js
+++ app/scripts/controllers/user-center/credit-security-details.js
@@ -2,7 +2,7 @@
  * @Author: fuqiang1
  * @Date:   2016-08-09 11:23:50
  * @Last Modified by:   fuqiang1
- * @Last Modified time: 2016-08-11 15:21:20
+ * @Last Modified time: 2016-08-11 16:06:42
  */
 
 'use strict';
@@ -29,7 +29,7 @@ angular.module('p2pSiteMobApp')
       // 年化收益率
       $scope.annualEarnings = $scope.creditRight.type == 1 ? $scope.project.annualEarnings : $scope.creditRight.baseRate + $scope.creditRight.riseRate;
       $scope.loanTime = $scope.creditRight.type == 1 ? $scope.project.loanTime : $scope.fundsProject.loanTime;
-      $scope.projectDays = $scope.creditRight.type == 1 ? $scope.project.projectDays: new Date($scope.fundsProject.repaymentDate - $scope.fundsProject.loanTime).getDate();
+      $scope.projectDays = $scope.creditRight.type == 1 ? $scope.project.projectDays : new Date($scope.fundsProject.repaymentDate - $scope.fundsProject.loanTime).getDate();
     });
 
 
