c3737bbcb99814792d0b6788b85b4c56a754a3bc
fuqiang <fuqiang1@hoolai.com>
2016-08-15 15:33:55 +0800

    fix(忘记密码) 点击获取短信验证码的条件修改

--- app/scripts/controllers/get-pwd-back.js
+++ app/scripts/controllers/get-pwd-back.js
@@ -19,16 +19,63 @@ angular.module('p2pSiteMobApp')
       /**
        * 判断手机号码
        */
-      if (!user.mobile || user.mobile.length !== 11||!mobilePattern.test(user.mobile)) {
+      if (!user.mobile || user.mobile.length !== 11 || !mobilePattern.test(user.mobile)) {
         $scope.msg = '手机号码格式不正确';
         $scope.showMsg();
         return;
       }
-      if(!user.picCaptcha) {
+      if (!user.picCaptcha || $scope.checkPic == false) {
         $scope.msg = '图形验证码有误';
         $scope.showMsg();
         return;
       }
+      /**
+       * 验证码倒计时
+       */
+      var captcha = document.getElementById("captcha");
+      function countDown(obj, second, inOrOut) {
+        var getMobile = document.getElementById("usermobile");
+        var mobilePattern = /^((13[0-9])|(15[^4,\D])|(18[0-9])|(17[0678])|(14[0-9]))\d{8}$/;
+        var buttonDefaultValue = captcha.innerHTML;
+        if (inOrOut === 'out' && window.buttonFlag == 0) {
+          return;
+        }
+        // 如果秒数还是大于0，则表示倒计时还没结束
+        if (mobilePattern.test(getMobile.value)) {
+          if (second >= 0) {
+            // 获取默认按钮上的文字
+
+            if (typeof buttonDefaultValue === 'undefined') {
+              buttonDefaultValue = obj.defaultValue;
+            }
+            // 按钮置为不可点击状态
+            obj.disabled = true;
+            window.buttonFlag = 0;
+            // 按钮里的内容呈现倒计时状态
+            //obj.value = buttonDefaultValue + '(' + second + ')';
+            obj.innerHTML = second + "s";
+            obj.className = '';
+            // 时间减一
+            second--;
+            // 一秒后重复执行
+            setTimeout(function() {
+              countDown(obj, second, 'in');
+            }, 1000);
+            // 否则，按钮重置为初始状态
+          } else {
+            // 按钮置为可点击状态
+            obj.disabled = false;
+            window.buttonFlag = 1;
+            // 按钮里的内容恢复初始状态
+            obj.className = '';
+            obj.innerHTML = "重新发送";
+          }
+        } else {
+          obj.disabled = true;
+          window.buttonFlag = 0;
+        }
+      }
+
       mobileCaptcha.$create({
         mobile: user.mobile
       }).$then(function(response) {
@@ -36,7 +83,9 @@ angular.module('p2pSiteMobApp')
           $scope.checkePicMsg = true;
           $scope.msg = response.msg;
           $scope.showMsg();
+
         }
+        countDown(captcha, 60, 'out');
       });
     };
     var mobilePattern = /^((13[0-9])|(15[^4,\D])|(18[0-9])|(17[0678]))\d{8}$/;
@@ -65,6 +114,7 @@ angular.module('p2pSiteMobApp')
       }
     })
     $scope.$watch('user.picCaptcha', function(oldVal) {
+      $scope.checkPic = false;
       if (oldVal !== undefined) {
         $scope.msg = '';
         var valLgth3 = oldVal.toString().length;
@@ -74,7 +124,7 @@ angular.module('p2pSiteMobApp')
             url: DEFAULT_DOMAIN + '/captchas/checkPic?captcha=' + oldVal
           }).success(function(data) {
             if (data == true) {
-              $scope.showMsg();
+              $scope.checkPic = true;
             } else {
               $scope.checkePicMsg = true;
               $scope.msg = '图形验证码错误';
@@ -105,7 +155,7 @@ angular.module('p2pSiteMobApp')
           $scope.msg = response.msg;
           $scope.showMsg();
         } else {
-          $state.go('root.getPwd2', {
+          $state.go('root.modifyPwd', {
             mobile: mobile,
             captcha: captcha
           });
@@ -132,29 +182,29 @@ angular.module('p2pSiteMobApp')
 
 
     //确认找回并修改密码
-    $scope.changePwd = function(pwd1, pwd2) {
-      if (!pwd1 || !pwd2) {
-        return;
-      }
+    // $scope.changePwd = function(pwd1, pwd2) {
+    //   if (!pwd1 || !pwd2) {
+    //     return;
+    //   }
 
-      if (pwd1 !== pwd2) {
-        $scope.changePasswordMsg = "两次密码输入不一致";
-        return;
-      }
+    //   if (pwd1 !== pwd2) {
+    //     $scope.changePasswordMsg = "两次密码输入不一致";
+    //     return;
+    //   }
 
-      restmod.model(DEFAULT_DOMAIN + '/users/resetMobilePassword')
-        .$create({
-          mobile: $scope.mobileNum,
-          captcha: $scope.captchaNum,
-          password: md5.createHash(pwd2)
-        }).$then(function(response) {
-          if (response.ret === -1) {
-            $scope.changePasswordMsg = response.msg;
-          } else {
-            $state.go('root.login');
-          }
-        });
-    }
+    //   restmod.model(DEFAULT_DOMAIN + '/users/resetMobilePassword')
+    //     .$create({
+    //       mobile: $scope.mobileNum,
+    //       captcha: $scope.captchaNum,
+    //       password: md5.createHash(pwd2)
+    //     }).$then(function(response) {
+    //       if (response.ret === -1) {
+    //         $scope.changePasswordMsg = response.msg;
+    //       } else {
+    //         $state.go('root.login');
+    //       }
+    //     });
+    // }
 
 
   });
