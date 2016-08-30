cc6039816e38581f225416395cc9ea3e0ada2138
yuyang <yuyang1@hoolai.com>
2016-08-30 14:59:15 +0800

    fix(register.js):注册页引用checkPicUtil图形验证码校验

--- app/scripts/services/checkPicUtil.js
+++ app/scripts/services/checkPicUtil.js
@@ -1,8 +1,8 @@
 /*
  * @Author: fuqiang1
  * @Date:   2016-08-29 17:43:41
- * @Last Modified by:   fuqiang1
- * @Last Modified time: 2016-08-30 09:01:28
+ * @Last Modified by:   yuyang
+ * @Last Modified time: 2016-08-30 14:52:25
  */
 
 'use strict';
@@ -10,23 +10,23 @@ angular.module('p2pSiteMobApp')
   .factory('CheckPicUtil', function($rootScope, DEFAULT_DOMAIN, $http) {
     return {
       checkePic: function(val) {
-        if (val !== undefined) {
-          var msg = '';
-          var valLgth = val.toString().length;
-          if (valLgth >= 4) {
-            $http({
-              method: 'POST',
-              url: DEFAULT_DOMAIN + '/captchas/checkPic?captcha=' + val
-            }).success(function(data) {
-              if (data == true) {} else {
-                msg = '图形验证码错误';
-                $rootScope.showMsg(msg);
-              }
-            }).error(function() {
+        if (val === undefined) {
+          return;
+        }
+        var msg = '';
+        if (val.length >= 4) {
+          $http({
+            method: 'POST',
+            url: DEFAULT_DOMAIN + '/captchas/checkPic?captcha=' + val
+          }).success(function(data) {
+            if (data == true) {} else {
               msg = '图形验证码错误';
               $rootScope.showMsg(msg);
-            });
-          }
+            }
+          }).error(function() {
+            msg = '图形验证码错误';
+            $rootScope.showMsg(msg);
+          });
         }
       }
     }
