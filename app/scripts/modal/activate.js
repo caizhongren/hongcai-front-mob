372a5ded8419125fc2d9e47aaa6231a5a1336a9e
fuqiang <fuqiang1@hoolai.com>
2016-09-19 15:00:44 +0800

    fix(activate) 存管通落地页按钮链接的问题

--- app/scripts/modal/activate.js
+++ app/scripts/modal/activate.js
@@ -2,12 +2,12 @@
  * @Author: Administrator
  * @Date:   2016-08-22 18:44:36
  * @Last Modified by:   fuqiang1
- * @Last Modified time: 2016-08-23 16:39:01
+ * @Last Modified time: 2016-09-19 14:58:21
  */
 
 'use strict';
 angular.module('p2pSiteMobApp')
-  .controller('ActivateCtrl', function($scope, $rootScope, $state, $uibModalInstance) {
+  .controller('ActivateCtrl', function($scope, $rootScope, $state) {
     $scope.showActivateTip = true;
     $scope.cancel = function() {
       $scope.showActivateTip = false;
@@ -16,7 +16,7 @@ angular.module('p2pSiteMobApp')
     /**
      * 立即迁移
      */
-    
+
     $scope.toOpen = function(){
     	$scope.cancel();
     	if(!$rootScope.isLogged){
@@ -25,9 +25,9 @@ angular.module('p2pSiteMobApp')
       	}
 
       	if($rootScope.securityStatus.realNameAuthStatus === 1 && !$rootScope.securityStatus.userAuth.active){
-      		$state.go("root.yeepay-transfer({type: 'active'})");
+      		$state.go("root.yeepay-transfer",{type: 'active'});
       	} else if($rootScope.securityStatus.realNameAuthStatus == 0){
-      		$state.go("root.yeepay-transfer({type: 'register'})");
+      		$state.go("root.yeepay-transfer",{type: 'register'});
       	}
 
     }
