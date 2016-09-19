b1ff15b474b64d26c66b21d550b5f92c10839091
yuyang <yuyang1@hoolai.com>
2016-09-19 10:29:40 +0800

    fix(register/login):注册登录页密码眼睛

--- app/scripts/directives/select-eyes.js
+++ app/scripts/directives/select-eyes.js
@@ -2,7 +2,7 @@
 * @Author: yuyang
 * @Date:   2016-08-31 09:58:58
 * @Last Modified by:   yuyang
-* @Last Modified time: 2016-08-31 14:35:33
+* @Last Modified time: 2016-09-19 10:27:53
 * @Effect 密码的显示与隐藏功能
 */
 
@@ -15,7 +15,7 @@ angular.module('p2pSiteMobApp')
           var eyes =true;
           var show = 'fa-eye';
           var hideen = 'fa-eye-slash';
-          var password = element[0].previousElementSibling;
+          var password = element[0].parentNode.previousElementSibling;
           function select(){
             if(eyes){
               element.removeClass(hideen).addClass(show);
