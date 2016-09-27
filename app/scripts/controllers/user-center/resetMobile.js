2a69f03d63edefe9d3c328a2ddeb56accfef2c86
fuqiang <fuqiang1@hoolai.com>
2016-09-27 18:24:04 +0800

    fix(修改手机号) 已经被占用但是提示图形验证码有误的问题

--- app/scripts/controllers/user-center/resetMobile.js
+++ app/scripts/controllers/user-center/resetMobile.js
@@ -2,7 +2,7 @@
 * @Author: fuqiang1
 * @Date:   2016-09-22 15:50:26
 * @Last Modified by:   fuqiang1
-* @Last Modified time: 2016-09-24 17:45:42
+* @Last Modified time: 2016-09-27 18:23:16
 */
 
 'use strict';
@@ -46,7 +46,7 @@
           return;
         }
 
-        if ($scope.piccha == false || picCaptcha.toString().length !== 4) {
+        if (picCaptcha.toString().length !== 4) {
           $rootScope.showMsg('图形验证码有误');
           return;
         }
@@ -62,7 +62,8 @@
         //判断手机号是否被占用,短信验证码是否正确
         Restangular.one('/users/').one('0/').post('resetMobile', {
           mobile: mobile,
-          captcha: captcha
+          captcha: captcha,
+          type: 1
         }).then(function(response) {
           if(response.ret === -1){
             $rootScope.showMsg(response.msg);
