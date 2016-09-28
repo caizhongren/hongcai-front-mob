012d2cf216d1f5081bf6d97db73a8ebda3714dd1
fuqiang <fuqiang1@hoolai.com>
2016-09-28 11:12:46 +0800

    fix(复制链接) 尝试解决在手机微信上不能复制链接的问题

--- app/scripts/modal/copy-link.js
+++ app/scripts/modal/copy-link.js
@@ -2,7 +2,7 @@
 * @Author: fuqiang1
 * @Date:   2016-09-27 17:16:52
 * @Last Modified by:   fuqiang1
-* @Last Modified time: 2016-09-28 10:30:09
+* @Last Modified time: 2016-09-28 11:09:33
 */
 
 'use strict';
@@ -19,7 +19,7 @@ angular.module('p2pSiteMobApp')
     $scope.btnInner = '复制链接';
     $scope.copyLink = function(){
       $scope.btnInner = '复制成功';
-      ngClipboard.toClipboard('http://www.hongcai.com/register?inviteCode=' + $scope.voucher.inviteCode);
+      // ngClipboard.toClipboard('http://www.hongcai.com/register?inviteCode=' + $scope.voucher.inviteCode);
     }
     /**
      * 邀请码
@@ -27,5 +27,15 @@ angular.module('p2pSiteMobApp')
     $scope.voucher = HongcaiUser.$find('0' + '/voucher').$then();
     //链接
     $scope.inviteUrl = "http://www.hongcai.com/register?inviteCode=" + $scope.voucher.inviteCode;
+
+    //实例化clipboard对象
+    // var ClipboardCopy = new Clipboard('#copy-btn');
+    /**
+     * 复制邀请链接
+     */
+    // $scope.copyInviteUrl = function(){
+
+
+    // }
 
   });
