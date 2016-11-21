75dc63cf7b5bc03cfc44448e8c7262d642803aac
yuyang <yuyang1@hoolai.com>
2016-11-21 13:13:14 +0800

    feat(assignment-detail.html):债权认购页面数据绑定

--- app/scripts/controllers/assignment/assignment-detail-ctrl.js
+++ app/scripts/controllers/assignment/assignment-detail-ctrl.js
@@ -7,49 +7,115 @@
  */
 'use strict';
 angular.module('p2pSiteMobApp')
-  .controller('AssignmentDetailCtrl', function($stateParams, Restangular, $scope, $rootScope) {
+  .controller('AssignmentDetailCtrl', function($state, DateUtils, $stateParams, Restangular, $scope, $rootScope) {
     var number = $stateParams.number; 
-    
+    $scope.lastRepayDay = 0;
+    $scope.currentStock = 0;
     /**
      * 债权转让信息详情
      */
     Restangular.one('assignments').one(number).get({}).then(function(response) {
       if(response && response.ret !== -1) {
         $scope.assignment = response; 
+        $scope.assignmentNum = $scope.assignment.number; 
         $scope.currentStock = response.currentStock;
-        $scope.projectNumber = response.projectNumber; 
+        $scope.projectNumber = response.projectNumber;
+        $scope.annual = response.annualEarnings; 
+        $scope.originalAnnual = response.originalAnnualEarnings;
+        $scope.remainDay = response.remainDay;
+       //原项目还款计划
+        Restangular.one('projects/'+$scope.projectNumber+'/projectBills').get({}).then(function(response){
+          if(response && response.ret !==-1) {
+            $scope.projectBills = response;
+            $scope.latestProjectBill;
+            for(var i= 0; i< response.length; i++) {
+              if(response[i].status === 0) {
+                $scope.latestProjectBill = response[i];
+                break;
+              }
+            }
+            $scope.lastRepayDay = $scope.latestProjectBill.lastRepaymentTime;
+            $scope.assignmentInvestAmount = 100;
+            //监测投资金额
+            $scope.$watch('assignmentInvestAmount', function(newVal, oldVal){
+              console.log($scope.currentStock);
+              if(!$rootScope.isLogged || $scope.currentStock <=0){
+                return;
+              }
+
+              if(newVal !== oldVal){
+                $scope.msg = undefined;
+              }
+
+              if($rootScope.account.balance <= 0){
+                $scope.msg = '账户余额不足，请先充值';
+              }
+
+              if(newVal){
+                if(newVal == 100 && $rootScope.account.balance >=100 && $scope.currentStock *100 >=100){
+                  $scope.msg = '';
+                }else if(newVal >  $rootScope.account.balance || (newVal <= $rootScope.account.balancenewVal && $rootScope.account.balancenewVal < $scope.realPayAmount)){
+                  $scope.msg = '账户余额不足，请先充值';
+                } else if(newVal < 100 ){
+                  $scope.msg = '投资金额必须大于100';
+                } else if(newVal % 100 !==0 ){
+                  $scope.msg = '投资金额必须为100的整数倍';
+                }else if(newVal > $scope.currentStock *100){
+                  $scope.msg = '投资金额必须小于' + $scope.currentStock *100;
+                }
+              }
+              $scope.showMsg();
+              //上次还款到认购当日的天数
+              var lastPayDays = DateUtils.intervalDays(new Date().getTime(), $scope.lastRepayDay); 
+              var reward = ($scope.annual - $scope.originalAnnual) * newVal * $scope.remainDay / 36500;
+              //  代收未收利息
+              $scope.exProfit = newVal * $scope.originalAnnual * lastPayDays / 36500;
+              //实际支付金额
+              $scope.realPayAmount = newVal + $scope.exProfit - reward;
+              //待收利息
+              $scope.profit = newVal * $scope.remainDay * $scope.annual / 36500;
+            });
+          }
+        });
       }
     });
-    
-    $scope.assignmentInvestAmount = 1000;
-    //监测投资金额
-    $scope.$watch('assignmentInvestAmount', function(newVal, oldVal){
-      if(!$rootScope.isLogged){
+
+    $scope.showMsg = function(){
+      if($scope.assignment && $scope.assignment.status == 1){
+        $rootScope.showMsg($scope.msg);
+      }
+    }
+
+    /**
+     * 下单并支付
+     */
+    $scope.clicked = true;
+    $scope.toInvest = function(assignmentNum, assignmentAmount) {
+      $scope.clicked = false;
+      if($scope.msg || assignmentAmount < 100){
         return;
       }
 
-      if(newVal !== oldVal){
-        $scope.msg = undefined;
-      }
-
-      if($rootScope.account.balance <= 0){
-        $scope.msg = '账户余额不足，请先充值';
-      }
-
-      if(newVal){
-        if(newVal > $scope.currentStock *100){
-          $scope.msg = '投资金额必须小于' + $scope.currentStock *100;
-        }else if(newVal > $rootScope.account.balance){
-          $scope.msg = '账户余额不足，请先充值';
-        } else if(newVal < 100 ){
-          $scope.msg = '投资金额必须大于100';
-        } else if(newVal % 100 !==0 ){
-          $scope.msg = '投资金额必须为100的整数倍';
-        }
-      }
-
       $scope.showMsg();
-    });
+      $rootScope.tofinishedOrder();
+      $rootScope.showLoadingToast = true;
+      Restangular.one('assignments/' + assignmentNum + '/orders' + '?amount=' + assignmentAmount).post('', {
+       
+      }).then(function(order){
+        $rootScope.showLoadingToast = false;
+        $scope.clicked = true;
+        // 重复下单后，response.number为undefined
+        if (order && order.ret !== -1) {
+          $state.go('root.yeepay-transfer', {
+            type: 'transfer',
+            number: order.number
+         });
+        } else {
+          $scope.msg = order.msg;
+          $scope.showMsg();
+        }
+      });
+    };
 
     /**
      * 修改投资金额
@@ -58,11 +124,6 @@ angular.module('p2pSiteMobApp')
       $event.stopPropagation();
       $scope.assignmentInvestAmount = $scope.assignmentInvestAmount ? $scope.assignmentInvestAmount + offset : offset;
       $scope.assignmentInvestAmount = $scope.assignmentInvestAmount < 100 ? 100 : $scope.assignmentInvestAmount;
-    }
-    /**
-     * 加载更多
-     */
-    
-
+    }    
 
   });
