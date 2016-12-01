488d8b5b0e1e1781a4861ee69478ea4469e21f79
fuqiang <fuqiang1@hoolai.com>
2016-12-01 14:39:41 +0800

    fix(autotender) 点击图标掉datepicker，存管通认证回调页面错乱

--- app/scripts/controllers/yeepay-callback.js
+++ app/scripts/controllers/yeepay-callback.js
@@ -8,7 +8,7 @@
  * Controller of the p2pSiteMobApp
  */
 angular.module('p2pSiteMobApp')
-  .controller('YeepayCallbackCtrl', function($rootScope, $scope, $state, $stateParams, Restangular, restmod, DEFAULT_DOMAIN) {
+  .controller('YeepayCallbackCtrl', function($rootScope, $scope, $state, $stateParams, Restangular, restmod, DEFAULT_DOMAIN, ipCookie) {
     $scope.page = '0';
     $scope.amount = $stateParams.amount;
     var business = $stateParams.business;
@@ -39,5 +39,5 @@ angular.module('p2pSiteMobApp')
       $scope.page = 10;
       $state.go('root.userCenter.autotender');
     } 
-
+    ipCookie('mark', 'callbackSuccess');
   });
