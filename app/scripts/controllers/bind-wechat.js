9ba8e90d6e73dafdf87d5c20653033e216389c4a
fuqiang <fuqiang1@hoolai.com>
2017-05-06 16:52:27 +0800

    fix(bind-wechat.js) 已绑定跳转页面

--- app/scripts/controllers/bind-wechat.js
+++ app/scripts/controllers/bind-wechat.js
@@ -18,23 +18,19 @@ angular.module('p2pSiteMobApp')
     * 查询用户绑定信息
     **/
     
-    Restangular.one('/activitys/isBindings').get({}).then(function(response){
+    Restangular.one('/activitys/isWechatSubscriptionBindings').get({}).then(function(response){
         if (!response || response.ret === -1) {
             return;
         }
         $scope.isBindWechat = response.bindingFlag;
+        if($scope.isBindWechat){
+           $state.go('root.bindWechat-status',{status:1}); 
+        }
     })
 
     //监测手机号码
     $scope.mobilePattern = /^((13[0-9])|(15[^4,\D])|(18[0-9])|(17[03678])|(14[0-9]))\d{8}$/;
-    $scope.$watch('wechatUser.mobile', function(newVal) {
-      if (newVal !== undefined) {
-          var valLgth = newVal.toString().length;
-          if (valLgth > 11 && !$scope.mobilePattern.test(newVal)) {
-            $rootScope.showMsg('手机号码格式不正确');
-          }
-        }
-    })
+    
     //去注册
     $scope.toRegister = function() {
       if (!$rootScope.timeout) {
@@ -55,10 +51,6 @@ angular.module('p2pSiteMobApp')
         if(!$scope.mobilePattern.test(wechatUser.mobile)) {
            $rootScope.showMsg('手机号码格式不正确');
            return; 
-        }
-        if($scope.isBindWechat){
-           $state.go('root.bindWechat-status',{status:1}); 
-           return;
         }
         $scope.busy = true;
         Restangular.one('activitys/').post('wechatSubscriptionBinding',{
