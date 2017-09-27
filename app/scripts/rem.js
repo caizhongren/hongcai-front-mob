3006717a005a63a9ed7802a210e72ad6d3342bdd
fuqiang <fuqiang@hoolai.com>
2017-09-27 10:42:27 +0800

    fix(common.css): 游戏页的公用 样式表

--- app/scripts/rem.js
+++ app/scripts/rem.js
@@ -1,12 +1,16 @@
-(function(doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function() {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
-        };
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+window.onload = function(){
+    function setRem() {
+        (document.documentElement.style.fontSize = (document.documentElement.clientWidth > 640 ? 100 : 100 * document.documentElement.clientWidth / 640) + "px")
+        window.addEventListener && (document.addEventListener("DOMContentLoaded",
+            function() {
+                setRem()
+            }), window.addEventListener("load",
+            function() {
+                setTimeout(setRem, 300)
+            }), window.addEventListener("resize",
+            function() {
+                setTimeout(setRem, 300)
+            }))
+    }
+    setRem();
+}()
