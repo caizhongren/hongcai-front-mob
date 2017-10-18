0f7c342a183bb6fad58cdd941f87d95052bebb05
yuyang <yuyang1@hooali.com>
2017-10-18 10:30:44 +0800

    fix(rem):添加横屏提示

--- app/styles/un-min/rem.js
+++ app/styles/un-min/rem.js
@@ -1,16 +1,20 @@
 window.onload = function(){
     function setRem() {
-        (document.documentElement.style.fontSize = (document.documentElement.clientWidth > 640 ? 100 : 100 * document.documentElement.clientWidth / 640) + "px")
-        window.addEventListener && (document.addEventListener("DOMContentLoaded",
-            function() {
-                setRem()
-            }), window.addEventListener("load",
-            function() {
-                setTimeout(setRem, 300)
-            }), window.addEventListener("resize",
-            function() {
-                setTimeout(setRem, 300)
-            }))
+      if(window.orientation!=0){
+        alert('亲，把手机竖起来游戏体验更好哦～')
+        return
+      }
+      (document.documentElement.style.fontSize = (document.documentElement.clientWidth > 640 ? 100 : 100 * document.documentElement.clientWidth / 640) + "px")
+      window.addEventListener && (document.addEventListener("DOMContentLoaded",
+      function() {
+          setRem()
+      }), window.addEventListener("load",
+      function() {
+          setTimeout(setRem, 300)
+      }), window.addEventListener("resize",
+      function() {
+          setTimeout(setRem, 300)
+      }))
     }
     setRem();
   function setHeight () {
